49219c6f2fd2992dfa67c6a9883da17782dcd996
ayahkhalilia <ayah.khalilia@stu.shenkar.ac.il>
2024-07-22 17:31:37 +0300

    checking connection

--- client/js_files/functions.js
+++ client/js_files/functions.js
@@ -1,7 +1,9 @@
 window.onload =()=>{
-
+   fetch("http://127.0.0.1:4000/data")
+     .then(response=>response.json())
+     .then(data=>showdata(data));
 }
 
-function fetch_branch_data(){
+/*function fetch_branch_data(){
     fetch("json_files/")
-}
+}*/
--- server/index.js
+++ server/index.js
@@ -1,3 +1,9 @@
 const express =require("express");
 const app=express();
-//const port=process.env.PORT || 3000;
+const port=process.env.PORT || 4000;
+app.get("/data",(req,res)=>{
+    console.log("got requst from client");
+    res.json();
+});
+app.listen(port);
+console.log(`listening on port ${port}`);
